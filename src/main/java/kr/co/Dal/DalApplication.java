78e6a0bf5786adf7c58f1f27c7788d888e7e5d33
이원정 <yiwonjeong@iwonjeong-ui-noteubug.local>
2023-10-09 15:53:06 +0900

    2023/10/09 yiwonjeong first git push test

---
+++ src/main/java/kr/co/Dal/DalApplication.java
@@ -0,0 +1,10 @@
+package kr.co.Dal;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
