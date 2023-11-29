481324012a04a11b57b556c7ec808ff00cdccd40
JeHyeongPark <qkrwpgud1996@gmail.com>
2023-11-29 13:36:16 +0900

    20231129JAY(1)

--- src/main/java/kr/co/Dal/my/web/MyController.java
+++ src/main/java/kr/co/Dal/my/web/MyController.java
@@ -14,5 +14,55 @@ public class MyController {
     public String mypagemain() {
         return "my/MyPageMain";
     }
+
+    // MY Board
+    @GetMapping("/my/MyBoard")
+    public String myboard(){
+        return "my/MyBoard";
+    }
+
+    // My Coupon
+    @GetMapping("/my/myCoupon")
+    public String mycoupon(){
+        return "my/MyCoupon";
+    }
+
+    // My Info
+    @GetMapping("/my/myInfo")
+    public String myinfo(){
+        return "my/MyInfo";
+    }
+
+    // My Order
+    @GetMapping("/my/myOrder")
+    public String myorder(){
+        return "my/MyOrder";
+    }
+
+    // My Point
+    @GetMapping("/my/myPoint")
+    public String mypoint(){
+        return "my/MyPoint";
+    }
+
+    // My Wish
+    @GetMapping("/my/myWish")
+    public String mywish(){
+        return "my/MyWish";
+    }
+
+    // My Withdraw
+    @GetMapping("/my/myWithdraw")
+    public String mywithdraw(){
+        return "my/MyWithdraw";
+    }
+
+
+
+
+
+
+
+
 }
 
