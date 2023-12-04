050d09cae18f8d744e3c8c081b3385411190d2b1
Project-DAL <drinkallliquor@gmail.com>
2023-12-04 17:59:35 +0900

    2023.12.14 김진형 커뮤니티 목록 업데이트

--- src/main/java/kr/co/Dal/comm/web/CommController.java
+++ src/main/java/kr/co/Dal/comm/web/CommController.java
@@ -1,4 +1,27 @@
 package kr.co.Dal.comm.web;
+/*
 
+ */
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/comm")
+@RequiredArgsConstructor
 public class CommController {
+
+    /**
+     * 커뮤니티 목록
+     */
+    @GetMapping("/commList")
+    public String commList() {
+        return "comm/commList";
+    }
+
+
 }
--- src/main/java/kr/co/Dal/my/web/MyController.java
+++ src/main/java/kr/co/Dal/my/web/MyController.java
@@ -1,89 +1,89 @@
-/*
-    파일명        : MyController
-    최초 작성자    : 박제형
-    최초 작성날짜   : 2023.11.28
-*/
-
-package kr.co.Dal.my.web;
-
-
-import kr.co.Dal.my.model.MyCouponVO;
-import kr.co.Dal.my.service.MyCouponService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-@RequiredArgsConstructor
-public class MyController {
-
-    private final MyCouponService myCouponService;
-
-    // My Page Main
-    @GetMapping("/my/MyPageMain")
-    public String mypagemain() {
-
-        return "my/MyPageMain";
-    }
-
-    // MY Board
-    @GetMapping("/my/MyBoard")
-    public String myboard(){
-
-        return "my/MyBoard";
-    }
-
-    // My Coupon
-    @GetMapping("/my/MyCoupon")
-    public String mycoupon(Model model){
-        MyCouponVO mycouponVO = new MyCouponVO();
-        model.addAttribute("couponList", myCouponService.selectCouponList(mycouponVO));
-
-        return "my/MyCoupon";
-    }
-
-    // My Info
-    @GetMapping("/my/MyInfo")
-    public String myinfo(){
-
-        return "my/MyInfo";
-    }
-
-    // My Order
-    @GetMapping("/my/MyOrder")
-    public String myorder(){
-
-        return "my/MyOrder";
-    }
-
-    // My Point
-    @GetMapping("/my/MyPoint")
-    public String mypoint(){
-
-        return "my/MyPoint";
-    }
-
-    // My Wish
-    @GetMapping("/my/MyWish")
-    public String mywish(){
-
-        return "my/MyWish";
-    }
-
-    // My Withdraw
-    @GetMapping("/my/MyWithdraw")
-    public String mywithdraw(){
-
-        return "my/MyWithdraw";
-    }
-
-
-
-
-
-
-
-
-}
-
+///*
+//    파일명        : MyController
+//    최초 작성자    : 박제형
+//    최초 작성날짜   : 2023.11.28
+//*/
+//
+//package kr.co.Dal.my.web;
+//
+//
+//import kr.co.Dal.my.model.MyCouponVO;
+//import kr.co.Dal.my.service.MyCouponService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class MyController {
+//
+//    private final MyCouponService myCouponService;
+//
+//    // My Page Main
+//    @GetMapping("/my/MyPageMain")
+//    public String mypagemain() {
+//
+//        return "my/MyPageMain";
+//    }
+//
+//    // MY Board
+//    @GetMapping("/my/MyBoard")
+//    public String myboard(){
+//
+//        return "my/MyBoard";
+//    }
+//
+//    // My Coupon
+//    @GetMapping("/my/MyCoupon")
+//    public String mycoupon(Model model){
+//        MyCouponVO mycouponVO = new MyCouponVO();
+//        model.addAttribute("couponList", myCouponService.selectCouponList(mycouponVO));
+//
+//        return "my/MyCoupon";
+//    }
+//
+//    // My Info
+//    @GetMapping("/my/MyInfo")
+//    public String myinfo(){
+//
+//        return "my/MyInfo";
+//    }
+//
+//    // My Order
+//    @GetMapping("/my/MyOrder")
+//    public String myorder(){
+//
+//        return "my/MyOrder";
+//    }
+//
+//    // My Point
+//    @GetMapping("/my/MyPoint")
+//    public String mypoint(){
+//
+//        return "my/MyPoint";
+//    }
+//
+//    // My Wish
+//    @GetMapping("/my/MyWish")
+//    public String mywish(){
+//
+//        return "my/MyWish";
+//    }
+//
+//    // My Withdraw
+//    @GetMapping("/my/MyWithdraw")
+//    public String mywithdraw(){
+//
+//        return "my/MyWithdraw";
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//}
+//
