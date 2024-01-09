8762091750bbb70e5ece90bf4c388ad897217e7c
Project-DAL <yjklp@naver.com>
2024-01-09 18:36:16 +0900

    20240109 신호준 아이디찾기,비번찾기 html 완성(조금 수정이 필요 할 수도 있음)

--- src/main/java/kr/co/Dal/user/controller/UserController.java
+++ src/main/java/kr/co/Dal/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package kr.co.Dal.user.controller;
 
-import com.google.api.Authentication;
 import kr.co.Dal.user.model.User;
 import kr.co.Dal.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +7,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class IndexController {
+public class UserController {
 
     @Autowired
     private UserRepository userRepository;
@@ -40,6 +37,16 @@ public class IndexController {
         user.setPassword(encPassword);
         userRepository.save(user);// 회원가입 잘됨. 비밀번호: 1234=> 시큐리티로 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안되있어서
         return "redirect:/loginForm";
+    }
+
+    @GetMapping("/findIdForm")
+    public  String findIdForm(){
+        return "user/findId";
+    }
+
+    @GetMapping("/findPwForm")
+    public  String findPwForm(){
+        return "user/findPw";
     }
 
 
