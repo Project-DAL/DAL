15f2f3e052f12ae1d0781ac9dd4065faa85524ca
Project-DAL <drinkallliquor@gmail.com>
2024-01-25 23:14:51 +0900

    20240125 yiwonjeong main

--- src/main/java/kr/co/Dal/main/mapper/MainMapper.java
+++ src/main/java/kr/co/Dal/main/mapper/MainMapper.java
@@ -2,22 +2,20 @@ package kr.co.Dal.main.mapper;
 
 import kr.co.Dal.main.model.MainVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Mapper
 public interface MainMapper {
 
-    /** 방법1) 반경 300km이내 검색 (Point type 사용 X) */
-    List<MainVO> selectStoreList(MainVO mainVO);
-
-    /** 방법2) 지도 각 모서리 좌표를 통해 데이터 검색 (Point type 사용) */
-    // 남서경도 남서위도 북동경도 북동위도
-    List<MainVO> selectStoreList2(MainVO mainVO);
+    /** 위치+주류 검색에 따른 검색 - 지도 각 모서리 좌표를 통해 데이터 검색 (Point type 사용) */
+    List<MainVO> selectStoreList(@Param("swLat") String swLat, @Param("swLng") String swLng, @Param("neLat") String neLat, @Param("neLng") String neLng);
 
     /** 셀렉트: 지역 가져오기 (location_tb) */
     List<MainVO> selectProvinceList(MainVO mainVO);
     List<MainVO> selectCityList(String province_id);
-    List<MainVO> selectTownList(String province_id, String city_id);
+    List<MainVO> selectTownList(@Param("province_id") String province_id, @Param("city_id") String city_id);
 
 }
--- src/main/java/kr/co/Dal/main/web/MainController.java
+++ src/main/java/kr/co/Dal/main/web/MainController.java
@@ -4,13 +4,16 @@ import kr.co.Dal.main.model.MainVO;
 import kr.co.Dal.main.service.MainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,7 +22,22 @@ public class MainController {
 
     private final MainService mainService;
 
-    /* 셀렉트: 지역 가져오기 (location_tb) */
+    /* 위치+주류 검색에 따른 검색 */
+    @GetMapping("/rest/storeList")
+    @ResponseBody
+    public ResponseEntity<List<MainVO>> selectStoreList(@Param("swLat") String swLat, @Param("swLng") String swLng,
+                                                        @Param("neLat") String neLat, @Param("neLng") String neLng) {
+
+        log.warn("selectStoreList controller");
+
+        log.warn("param : " + swLat + "," + swLng + "," + neLat + "," + neLng);
+
+        List<MainVO> storeList = mainService.selectStoreList(swLat, swLng, neLat, neLng);
+
+        return ResponseEntity.ok().body(storeList);
+    }
+
+    /* 셀렉트: 지역 가져오기 (location_tb)aa */
     @GetMapping("/rest/provinceList")
     @ResponseBody
     public ResponseEntity<List<MainVO>> selectProvinceList(MainVO mainVO) {
