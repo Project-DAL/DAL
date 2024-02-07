249c966c72e6c380f25581e23a8dbff5529d8de6
JeHyeongPark <qkrwpgud1996@gmail.com>
2024-02-08 01:12:17 +0900

    JAY20240207

--- src/main/resources/static/my/js/myMain.js
+++ src/main/resources/static/my/js/myMain.js
@@ -12,6 +12,7 @@ let selectedOption = orderDateSelect.options[orderDateSelect.selectedIndex].text
 
 document.addEventListener('DOMContentLoaded', function(){
     addEventListenerBtn();
+    fnAjaxList();
 })
 
 
@@ -26,18 +27,34 @@ function addEventListenerBtn(){
 
 function fnselectData(){
     console.log('selectedOption: ' + selectedOption);
-
-
-
-
-
-
-    /*
-       let jsonData = {
-
-       }
-
-    ajaxAPI("/my/MyWithdraw/deleteUser", null, "POST").then(response => {
-
-    });*/
 }
+
+
+
+function fnAjaxList() {
+
+    let jsonData = {}
+
+
+    ajaxAPI("/my/MyPageMain/order", null, "GET").then(response => {
+        console.log(response);
+        console.log("response length: " + response.length); // 각 주문번호
+
+        for(let i = 0; i < response.length; i++) {
+            // 예를 들어 주문번호 1번
+            console.log("response[0]: ", response[0]);
+            console.log("rdate: " + response[i].order_rdates);
+            console.log("rdate length: " + response[i].order_rdates.split(",").length);
+            let rdateLength = response[i].order_rdates.split(",").length;
+            let prodTits = response[i].prod_tits;
+
+            // 주문번호 1번 + 각 주문 제품
+            for (let j = 0; j < rdateLength; j++) {
+                console.log("prodTit: " + prodTits.split(",")[j]);
+            }
+
+        }
+
+    });
+}
+
