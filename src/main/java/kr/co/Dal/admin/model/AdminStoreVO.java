d2bcb59ca04977d6fe22fe086c4bb9b461d47bab
Project-DAL <drinkalliquor@gmail.com>
2024-02-11 00:13:38 +0900

    카멜케이스 추가

--- src/main/java/kr/co/Dal/admin/model/AdminStoreVO.java
+++ src/main/java/kr/co/Dal/admin/model/AdminStoreVO.java
@@ -6,4 +6,16 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class AdminStoreVO {
+    private int prodId;
+    private String prodTit;
+    private int prodType;
+    private int prodCnt;
+    private int prodOriPrice;
+    private int prodSalePrice;
+    private String prodImg;
+    private int prodStts;
+    private int prodLike;
+    private String prodRdate;
+    private String prodUdate;
+    private String prodCnFile;
 }
--- src/main/resources/static/admin/store/js/storeList.js
+++ src/main/resources/static/admin/store/js/storeList.js
@@ -15,6 +15,7 @@ Ver.  Date          Revised By   Description
 /* 1. Variables *******************************************************************************************************/
 /* 1.1 Etc Variables (Json Object, HTML String, Temporary) */
 let iSelectedId     = null; // 선택된 행의 ID
+let jsonGridList    = null;
 
 
 /* 2. Functions - Initialize ******************************************************************************************/
@@ -27,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // addEventListenerSearchCondition();
 
     /** 3) 최초 목록조회 실행 */
+    fnaAjaxList();
 });
 
 
@@ -36,8 +38,14 @@ function fnInsert() {
     window.location.href = "/admin/store/storeWrite";
 }
 
-function fnView() {
-    // appendGridListHtml();
+function fnaAjaxList() {
+    ajaxAPI('/admin/storeList',null,"GET").then(response => {
+        console.log("response: ", response);
+
+        jsonGridList = response.list;
+
+        appendGridListHtml();
+    })
 }
 
 
@@ -61,53 +69,67 @@ function addEventListenerCRUDBtn() {
 
 /* 5. Functions - Html Creator ****************************************************************************************/
 /* 5.1 appendGridListHtml() - 목록 html 생성 */
-// function appendGridListHtml() {
-//     /** 1) 각 btn, inputText, label, editor 값 초기화 */
-//     iSelectedId                 = '';       // 선택된 ID 초기화
-//     document.getElementById("main-table-tbody").innerHTML     = '';       // 게시물 제목 초기화
-//
-//     /** 2) 목록 생성 */
-//     if(jsonGridList.length > 0) {
-//         for(let item of jsonGridList) {
-//             /** 2-1) gridArea에 <tr> 요소생성 및 추가 */
-//             tbodyObjGridArea.insertAdjacentHTML('beforeend', `
-//                 <tr>
-//                     <td class="label-td"><input type="checkbox" name="chk" class="ck"></td>
-//                     <td class="label-td left"><div class="ellp">${htmlStrImg} ${item.bardSbjt}</div></td>
-//                     <td class="label-td">${item.rgstId}</td>
-//                     <td class="label-td">${item.rgstDt}</td>
-//                 </tr>`);
-//
-//             /** 2-2) gridArea에 Event Listener 등록 */
-//             tbodyObjGridArea.querySelector('tr:last-child').addEventListener('click', function() {
-//                 /** 2-2-1) 전체 checkbox cheked 해제 */
-//                 tbodyObjGridArea.querySelectorAll('input[type="checkbox"]')
-//                     .forEach((checkbox) => { checkbox.checked = false; })
-//
-//                 /** 2-2-2) 전체 checkbox 하일라이트 calss 해제 */
-//                 tbodyObjGridArea.querySelectorAll('tr')
-//                     .forEach((tr) => { tr.classList.remove('active'); });
-//
-//                 /** 2-2-3) 현재 행 체크박스 하이라이트 설정 */
-//                 this.querySelector('input[type="checkbox"]').checked = true;
-//                 this.classList.add('active');
-//
-//                 /** 2-2-4) 전역변수로 선택된 item 번호 세팅 */
-//                 iSelectedId = item.bardId;
-//
-//                 if(inputObjGblAuthCd.value == "USER"){
-//                     btnUpdate.style.display     = 'none';   // 수정버튼 숨김
-//                     btnDelete.style.display     = 'none';   // 삭제버튼 숨김
-//                 }else {
-//                     btnUpdate.style.display     = 'inline-block';   // 수정버튼
-//                     btnDelete.style.display     = 'inline-block';   // 삭제버튼
-//                 }
-//                 /** 2-2-5) 상세조회 실행 */
-//                 fnView();
-//             });
-//         }
-//         /** 3) gridArea에 검색결과 없음 <tr> 추가 */
-//     }
-// }
+function appendGridListHtml() {
+    /** 1) 각 btn, inputText, label, editor 값 초기화 */
+    iSelectedId                 = '';       // 선택된 ID 초기화
+    document.getElementById("main-table-tbody").innerHTML     = '';       // 게시물 제목 초기화
+
+    /** 2) 목록 생성 */
+    if(jsonGridList.length > 0) {
+        for(let item of jsonGridList) {
+            document.getElementById("main-table-tbody").insertAdjacentHTML('beforeend', `
+                 <tr>
+                                        <td>${item.prodId}</td>
+                                        <td>${item.prodTit}</td>
+                                        <td>${item.prodSalePrice}</td>
+                                        <td>${item.prodType}</td>
+                                        <td>${item.prodStts}</td>
+                                        <td>${item.prodCnt}</td>
+                                        <td>${item.prodRdate}</td>
+                                        <td>${item.prodUdate}</td>
+                                    </tr>`);
+            document.getElementById('tr:last-child').addEventListener('click', function () {
+
+            })
+            // /** 2-1) gridArea에 <tr> 요소생성 및 추가 */
+            // tbodyObjGridArea.insertAdjacentHTML('beforeend', `
+            //     <tr>
+            //         <td class="label-td"><input type="checkbox" name="chk" class="ck"></td>
+            //         <td class="label-td left"><div class="ellp">${htmlStrImg} ${item.bardSbjt}</div></td>
+            //         <td class="label-td">${item.rgstId}</td>
+            //         <td class="label-td">${item.rgstDt}</td>
+            //     </tr>`);
+            //
+            // /** 2-2) gridArea에 Event Listener 등록 */
+            // tbodyObjGridArea.querySelector('tr:last-child').addEventListener('click', function() {
+            //     /** 2-2-1) 전체 checkbox cheked 해제 */
+            //     tbodyObjGridArea.querySelectorAll('input[type="checkbox"]')
+            //         .forEach((checkbox) => { checkbox.checked = false; })
+            //
+            //     /** 2-2-2) 전체 checkbox 하일라이트 calss 해제 */
+            //     tbodyObjGridArea.querySelectorAll('tr')
+            //         .forEach((tr) => { tr.classList.remove('active'); });
+            //
+            //     /** 2-2-3) 현재 행 체크박스 하이라이트 설정 */
+            //     this.querySelector('input[type="checkbox"]').checked = true;
+            //     this.classList.add('active');
+            //
+            //     /** 2-2-4) 전역변수로 선택된 item 번호 세팅 */
+            //     iSelectedId = item.bardId;
+            //
+            //     if(inputObjGblAuthCd.value == "USER"){
+            //         btnUpdate.style.display     = 'none';   // 수정버튼 숨김
+            //         btnDelete.style.display     = 'none';   // 삭제버튼 숨김
+            //     }else {
+            //         btnUpdate.style.display     = 'inline-block';   // 수정버튼
+            //         btnDelete.style.display     = 'inline-block';   // 삭제버튼
+            //     }
+            //     /** 2-2-5) 상세조회 실행 */
+            //     fnView();
+            // });
+        }
+        /** 3) gridArea에 검색결과 없음 <tr> 추가 */
+    }
+}
 
 
