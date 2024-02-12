2464f2bfb653b527050311f245f396386cf6a16c
Project-DAL <drinkallliquor@gmail.com>
2024-02-13 00:30:01 +0900

    240212 storeQna 문의글작성 ajax, 유효성검사 완료, storeView navbar 고정 스크립트 추가

--- src/main/resources/static/store/js/storeView.js
+++ src/main/resources/static/store/js/storeView.js
@@ -10,6 +10,29 @@ Ver.  Date          Revised By   Description
 0.1   2024.01.14    김석진       최초개발
 ----------------------------------------------------------------------------------------------------------------------->
 */
-// function fnOpenPopup() {
-//     window.open("storeQnaWrite", "popup","width:400px, height:300px");
-// }
+let midbar = document.getElementById('ct_midbar');
+
+// 메뉴바의 위치 계산
+let midbarTop = midbar.offsetTop;
+let midbarHeight = midbar.offsetHeight;
+
+// 메뉴바의 너비 계산
+let midbarWidth = midbar.offsetWidth;
+
+// 스크롤 이벤트 핸들러
+window.addEventListener("scroll", function() {
+
+    // 스크롤하는 높이를 체크
+    let scrollPosition = window.scrollY;
+
+    // 스크롤하는 위치가 메뉴바보다 클 경우 (더 아래로 스크롤 할 경우)
+    if (scrollPosition >= midbarTop) {
+        console.log("midbarTop = " + midbarTop + ", scroll = " + scrollPosition);
+        midbar.style.position = "fixed";
+        midbar.style.width = midbarWidth + "px";
+        midbar.style.top = "40px";
+    } else {
+        // 메뉴바를 다시 원래 속성으로 변경
+        midbar.style.position = "static";
+    }
+});
